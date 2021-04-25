5642b40df9983c91327c6a9e134dffe87b92848e
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2021-04-24 20:49:19 -0500

    fix sh

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 git clone https://github.com/pin3da/notebook
-../bin/notebookgen ./notebook --author "Universidad Tecnologica de Pereira" --initials UTP -s 8 -c 3
+./bin/notebookgen ./notebook --author "Universidad Tecnologica de Pereira" --initials UTP -s 8 -c 3
